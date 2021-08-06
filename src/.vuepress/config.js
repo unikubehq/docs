2c286cb162261e65cd9642c92664830bcbe1e064
Tobias Frölich <40638719+tobifroe@users.noreply.github.com>
2021-08-06 16:07:52 +0200

    fix: favicon path

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -23,7 +23,8 @@ module.exports = {
   head: [
     ['meta', { name: 'theme-color', content: '#45d093' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'icon', href: '/favicon.ico'}]
   ],
 
   /**
