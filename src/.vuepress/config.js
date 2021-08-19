cb17028830ac9c04edf059cb5b38bd59ac6cfb07
Melanie Hielscher <86047585+marty-2015@users.noreply.github.com>
2021-08-19 11:13:41 +0200

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -75,6 +75,7 @@ module.exports = {
               'containerization',
               'continuos-integration',
               'docker',
+              'microservices',
             ]
           }
       ],
