90ef79c95a32e8d302facce8a7f0dbec62fd900c
Robert Stein <SteinRobert@users.noreply.github.com>
2021-09-24 12:13:41 +0200

    chore: add email service to roadmap

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -99,6 +99,7 @@ module.exports = {
               'osx-brew',
               'project-health-check',
               'remote-clusters',
+              'email-service',
             ]
           },
       ],
---
+++ src/roadmap/email-service.md
@@ -0,0 +1,10 @@
+---
+status: voting
+---
+
+# Email Service
+
+**Summary**
+
+Internal service for sending emails. Can be consumed by other services to send emails like
+password reset, invite etc.
