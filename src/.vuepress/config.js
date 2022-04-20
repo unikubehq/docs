c0352d1d324cb62bebc418a254137d716776e28d
Tobias Frölich <tobias@blueshoe.de>
2022-04-20 15:10:38 +0200

    feat: add logrocket

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -24,6 +24,10 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
+    ['script', { src: 'https://cdn.lr-in.com/LogRocket.min.js' }],
+    ['script', {}, `
+      window.LogRocket && window.LogRocket.init('gbnx0f/unikube', {rootHostname: 'unikube.io'});
+    `],
     ['script', {}, `
       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-MKVRSF6');
     `],
